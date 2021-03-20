3be0732639a77aaf51824270feb25f60974dce47
David Imboden <david.imboden@iteration.info>
2021-03-20 08:53:12 +0100

    2.3.1

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Default class for Sagenda Calendar module
  * 
@@ -18,17 +19,25 @@ defined('_JEXEC') or die; ?>
   <script>
     window.sagendaIntegration = 'Joomla';
     window.baseUrl = 'https://sagenda.net/api/v3/';
-    window.sagendaDefaultView = '<?php echo $sagendaDefaultView ;?>';
-    window.sagendaRemoveMonthViewButton = '<?php echo $sagendaRemoveMonthViewButton ;?>';
-    window.sagendaRemoveWeekViewButton = '<?php echo $sagendaRemoveWeekViewButton ;?>';
-    window.sagendaRemoveDayViewButton = '<?php echo $sagendaRemoveDayViewButton ;?>';
-    window.sagendaRemoveAgendaViewButton = '<?php echo $sagendaRemoveAgendaViewButton ;?>';
-    window.sagendaBearerToken = '<?php echo $sagendaBearerToken ;?>';
-    window.sagendaWeekStartsOn = '<?php echo $sagendaWeekStartsOn ;?>';
-    window.sagendaLanguageCultureShortName = '<?php echo $locale ;?>';
-    window.sagendaDateFormat = '<?php echo $sagendaDateFormat ;?>';  
-    window.sagendaTimeFormat = '<?php echo $sagendaTimeFormat ;?>'; 
+    window.sagendaDefaultView = '<?php echo $sagendaDefaultView; ?>';
+    window.sagendaRemoveMonthViewButton = '<?php echo $sagendaRemoveMonthViewButton; ?>';
+    window.sagendaRemoveWeekViewButton = '<?php echo $sagendaRemoveWeekViewButton; ?>';
+    window.sagendaRemoveDayViewButton = '<?php echo $sagendaRemoveDayViewButton; ?>';
+    window.sagendaRemoveAgendaViewButton = '<?php echo $sagendaRemoveAgendaViewButton; ?>';
+    window.sagendaBearerToken = '<?php echo $sagendaBearerToken; ?>';
+    window.sagendaWeekStartsOn = '<?php echo $sagendaWeekStartsOn; ?>';
+    window.sagendaLanguageCultureShortName = '<?php echo $locale; ?>';
+    window.sagendaDateFormat = '<?php echo $sagendaDateFormat; ?>';
+    window.sagendaTimeFormat = '<?php echo $sagendaTimeFormat; ?>';
     window.sagendaAngularPath = '<?php echo $module_path; ?>/angular/';
+    
+    //added this because of different scroll behaviour on Mac and Windows
+    if (window.navigator.userAgent.indexOf("Mac") === -1) {
+      let link = document.createElement('link');
+      link.rel = 'stylesheet';
+      link.href = '<?php echo $module_path; ?>/angular/scroll.css';
+      document.getElementsByTagName("head")[0].appendChild(link);
+    }
   </script>
 
   <script type="text/javascript" src="<?php echo $module_path; ?>/angular/runtime.js"></script>
